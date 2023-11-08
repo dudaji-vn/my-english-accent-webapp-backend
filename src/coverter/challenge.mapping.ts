99b33526be97039d33321203bee29916377682cb
Linh <hoquanglinh2710@gmail.com>
2023-11-08 08:59:26 +0700

    Add missing challenge api

---
+++ src/coverter/challenge.mapping.ts
@@ -0,0 +1,53 @@
+import {
+  IChallengeDetailDisplay,
+  IChallengeDisplay
+} from '../interfaces/dto/challenge.dto'
+
+export function convertToChallengeDisplayDTO(item: any): IChallengeDisplay {
+  return {
+    challengeId: item._id,
+    challengeName: item.challenge_name,
+    clubId: item.club,
+    created: item.created,
+    participants: item.participants,
+    updated: item.updated,
+    vocabularies: item.vocabularies.map((voca: any) => {
+      return {
+        challengeId: voca.challenge,
+        vocabularyId: voca.vocabulary,
+        updated: voca.updated,
+        created: voca.created,
+        number: voca.number,
+        clubVocabularyId: voca._id
+      }
+    })
+  }
+}
+
+export function convertToDetailChallengeDTO(
+  item: any
+): IChallengeDetailDisplay {
+  return {
+    challengeId: item._id,
+    challengeName: item.challenge_name,
+    clubId: item.club,
+    created: item.created,
+    participants: item.participants,
+    updated: item.updated,
+    vocabularies: item.vocabularies.map((voca: any) => {
+      return {
+        challengeId: voca.challenge,
+        vocabularyId: voca.vocabulary,
+        updated: voca.updated,
+        created: voca.created,
+        number: voca.number,
+        clubVocabularyId: voca._id,
+        vCreated: voca.created,
+        vUpdated: voca.updated,
+        vphoneticDisplayLanguage: voca.title_display_language,
+        vtitleDisplayLanguage: voca.phonetic_display_language,
+        lectureId: voca.lecture
+      }
+    })
+  }
+}
