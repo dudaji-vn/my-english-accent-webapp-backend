108be65d0e889c3c583957fb69d33428ad198e75
Linh <hoquanglinh2710@gmail.com>
2023-11-17 13:27:33 +0700

    Import lectures and vocabularies from excel

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,59 +1,53 @@
-import { injectable } from 'tsyringe'
-import fs from 'fs'
 import * as fastCsv from 'fast-csv'
+import fs from 'fs'
+import { injectable } from 'tsyringe'
+import LectureModel from '../entities/Lecture'
 import VocabularyModel from '../entities/Vocabulary'
+import { ICreateVocabularyDAO } from '../interfaces/dao/vocabulary.dao'
+import { IVocabularyRow } from '../interfaces/dto/vocabulary.dto'
 
 @injectable()
 export default class FileService {
-  private async parseCsvRow(row: string[], category: string) {
+  private async parseCsvRow(
+    row: string[]
+  ): Promise<IVocabularyRow | undefined> {
     const [
+      numberOrder,
+      lectureName,
       wordOrExpression,
-      pronunciation,
+      phonetic,
       textKorean,
-      textVietNam,
-      example,
-      exampleVI,
-      exampleKR,
-      wordType
+      textVietNam
     ] = row
 
     if (
+      parseInt(numberOrder) > 0 &&
+      lectureName &&
       wordOrExpression &&
-      pronunciation &&
       textKorean &&
       textVietNam &&
-      example &&
-      exampleVI &&
-      exampleKR &&
-      wordType &&
-      wordOrExpression !== 'word/expression'
+      phonetic
     ) {
       return {
-        text: {
-          en: wordOrExpression,
-          vi: textVietNam,
-          ko: textKorean
+        numberOrder: parseInt(numberOrder),
+        textTranslate: {
+          vn: textVietNam,
+          kr: textKorean
         },
-        pronunciation,
-        example: {
-          en: example,
-          vi: exampleVI,
-          ko: exampleKR
-        },
-        type: wordType,
-        category
+        titleDisplayLanguage: wordOrExpression,
+        lectureName: lectureName,
+        phonetic: phonetic
       }
     }
-    return null
   }
 
-  private async processCsvFile(source: string, category: string) {
+  private async processCsvFile(source: string): Promise<IVocabularyRow[]> {
     const stream = fs.createReadStream(source)
     const parser = fastCsv.parseStream(stream)
-    const listItems: any[] = []
+    const listItems: IVocabularyRow[] = []
 
     parser.on('data', async (row: string[]) => {
-      const parsedRow = await this.parseCsvRow(row, category)
+      const parsedRow = await this.parseCsvRow(row)
       if (parsedRow) {
         listItems.push(parsedRow)
       }
@@ -66,37 +60,69 @@ export default class FileService {
     return listItems
   }
 
-  private async insertDataIntoDatabase(data: any[]) {
-    try {
-      const result = await VocabularyModel.insertMany(data)
-      console.log('Documents inserted:', result)
-    } catch (err) {
-      console.error('Error inserting documents:', err)
+  async importDataFromExcel() {
+    const result = await this.handleStoreFile(
+      './resource/MEA-Voca-Lecture-23-11.csv'
+    )
+
+    return result
+  }
+
+  private async handleStoreFile(source: string) {
+    const vocabulariesData = await this.processCsvFile(source)
+    let listLectureName = vocabulariesData.map((item) => item.lectureName)
+    const existedLectures = (await LectureModel.find().lean()).map(
+      (item) => item.lecture_name
+    )
+    const needSaveLectures = [...new Set(listLectureName)]
+      .filter((lecture) => !existedLectures.includes(lecture))
+      .map((item) => {
+        return {
+          lecture_name: item
+        }
+      })
+
+    if (needSaveLectures && needSaveLectures.length > 0) {
+      await LectureModel.insertMany(needSaveLectures)
     }
-  }
+    const lectures = await LectureModel.find().lean()
 
-  async syncDataFromExcel() {
-    await VocabularyModel.deleteMany()
-    await this.handleStoreFile(
-      './resource/MEA_Voca_General.csv',
-      'general'
-    )
-    await this.handleStoreFile(
-      './resource/MEA_Voca_Designer.csv',
-      'designer'
-    )
-    await this.handleStoreFile(
-      './resource/MEA_Voca_Develops.csv',
-      'developer'
-    )
-    return 'sync data'
-  }
+    const existedVocabularies = await VocabularyModel.find().lean()
 
-  private async handleStoreFile(source: string, category: string) {
-    const generalData = await this.processCsvFile(source, category)
-    const sortedData = generalData.sort((a, b) =>
-      a.text.en.toLowerCase().localeCompare(b.text.en.toLowerCase())
-    )
-    await this.insertDataIntoDatabase(sortedData)
+    const vocabulariesNeedSave: (ICreateVocabularyDAO | undefined)[] =
+      vocabulariesData
+        .map((voca) => {
+          const lectureId = lectures
+            .find((lecture) => lecture.lecture_name === voca.lectureName)
+            ?._id.toString()
+
+          if (lectureId) {
+            return {
+              lecture: lectureId,
+              phonetic_display_language: voca.phonetic,
+              text_translate: voca.textTranslate,
+              title_display_language: voca.titleDisplayLanguage,
+              number_order: voca.numberOrder
+            }
+          }
+        })
+        .filter(
+          (item) =>
+            item?.lecture &&
+            item.number_order &&
+            item.phonetic_display_language &&
+            item.text_translate &&
+            item.title_display_language
+        )
+        .filter(
+          (item) =>
+            !existedVocabularies.find(
+              (voca) =>
+                voca.title_display_language === item?.title_display_language &&
+                voca?.lecture?.toString() === item.lecture
+            )
+        )
+    const vocaImports = await VocabularyModel.insertMany(vocabulariesNeedSave)
+    return vocaImports
   }
 }
