9ac0d897aa516fa82cab89ba7a245015890ebd95
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-08 13:16:10 +0700

    Add api for import vocabularies and upload file

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,8 +1,20 @@
 import { injectable } from 'tsyringe'
 import { IRequest, IResponse } from '../interfaces/common'
 import FileService from '../services/file.service'
+import multer from 'multer'
+import { BadRequestError } from '../middleware/error'
 
 @injectable()
 export default class FileController {
   constructor(private readonly fileService: FileService) {}
+  async uploadLectureAndVocabularyFromCsv(req: IRequest, res: IResponse) {
+    if (!req.file) {
+      throw new BadRequestError('csvFile is required')
+    }
+    const data = await this.fileService.uploadLectureAndVocabularyFromCsv(
+      req.file
+    )
+
+    return res.success(data)
+  }
 }
--- src/router/v1/file.router.ts
+++ src/router/v1/file.router.ts
@@ -1,6 +1,23 @@
 // @ts-nocheck
 import express from 'express'
+import { container } from 'tsyringe'
+
+import { catchAsync } from '../../middleware/catchAsync'
+import FileController from '../../controllers/file.controller'
+const fileController = container.resolve<FileController>(FileController)
+import multer from 'multer'
+import auth from '../../middleware/auth'
+import uploadFile from '../../middleware/uploadFile'
 
 const fileRouter = express.Router()
+
+fileRouter.put(
+  '/uploadLectureAndVocabularyFromCsv',
+  auth,
+  uploadFile('csvFile'),
+  catchAsync(
+    fileController.uploadLectureAndVocabularyFromCsv.bind(fileController)
+  )
+)
 
 export default fileRouter
